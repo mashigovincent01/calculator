f12eee2c0ee34c582ad449b90e803e8c1871f26d
mashigovincent01 <mashigovincent407@gmail.com>
2023-12-30 14:21:03 +0200

    digits display outupt

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 import calculator from "./calculator.js";
 var num1 = 0, num2 = 0, operator = "";
+const digits = document.getElementsByClassName("digit");
+const displayOutput = document.getElementById("display"); 
+var displayOutputValue = "";
+for(let i = 0; i < digits.length; i++){
+    digits[i].addEventListener("click", ()=>{
+        displayOutputValue += digits[i].innerHTML;
+        displayOutput.innerHTML = displayOutputValue;
+    });
+}
 
 function operate(op, n1, n2){
     if(op === '+'){
